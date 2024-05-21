9a4ddb4f587858b139d7a56672d1b628c8000186
Alex <aekazak@yandex.ru>
2024-05-21 19:24:13 +0300

    useFiltersUrlParamsService updated

--- src/features/filters/services/useFiltersUrlParamsService.ts
+++ src/features/filters/services/useFiltersUrlParamsService.ts
@@ -1,26 +1,37 @@
 import { useCallback } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-import useUrlService from '@services/useUrlService';
-
-import manageFilterUrlParam from './manage-filter-url-param';
+import { URL_PARAMS, URL_PARAMS_ORDER } from '@config/url-params';
 
 const useFiltersUrlParamsService = () => {
-  const { resetFiltersParams, updateFiltersParams } = useUrlService();
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const updateParams = useCallback(
     (eventTarget: EventTarget, param: string) => {
-      manageFilterUrlParam(searchParams, eventTarget, param);
-      updateFiltersParams(searchParams);
+      const selectedOption = (
+        (eventTarget as Element)?.closest('li') as HTMLElement
+      )?.dataset?.value;
+
+      if (!selectedOption) searchParams.delete(param);
+      if (selectedOption) searchParams.set(param, selectedOption);
+
+      const urlParamsInOrder = URL_PARAMS_ORDER.map(param =>
+        searchParams.has(param) ? `${param}=${searchParams.get(param)}` : ''
+      ).join('&');
+
+      setSearchParams(urlParamsInOrder);
     },
-    [searchParams, updateFiltersParams],
+    [searchParams, setSearchParams]
   );
 
-  const resetParams = useCallback(
-    () => resetFiltersParams(),
-    [resetFiltersParams],
-  );
+  const resetParams = useCallback(() => {
+    searchParams.delete(URL_PARAMS.country);
+    searchParams.delete(URL_PARAMS.genre);
+    searchParams.delete(URL_PARAMS.year);
+    searchParams.delete(URL_PARAMS.page);
+
+    setSearchParams(searchParams);
+  }, []);
 
   return { updateParams, resetParams };
 };
